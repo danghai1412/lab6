f4204f063a27170e46f3abcd16f17f1437225701
danghai1412 <haipdgch200436@fpt.edu.vn>
2022-09-29 14:27:37 +0700

    ok

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 //set thư mục chứa views (giao diện - frontend)
 app.set("views", path.join(__dirname, "/views"));
 //set view engine (template engine)
-app.set("view engine", "hbs");
+app.set("view engine", "pug");
 
 //render ra trang home (index.hbs)
 //get: get method
@@ -17,14 +17,18 @@ app.get("/", (req, res) => {
   res.render("index", { n: name, a: address });
 });
 
-
 app.get("/city", (req, res) => {
-    let cities = ["Hanoi", "HCM City", "Da Nang", "Can Tho"];
-    res.render("city", { cities: cities });
+  //let country = "Vietnam";
+  let cities = ["Hanoi", "HCM City", "Da Nang", "Can Tho"];
+  let providers = ["FPT", "Viettel", "VNPT"];
+  res.render("city", {
+    country: "Vietnam",
+    cities: cities,
+    providers: providers,
   });
-  
+});
+
 //chạy web server
 app.listen(port, () => {
   console.log("Server is running at: http://localhost:3000");
 });
-
